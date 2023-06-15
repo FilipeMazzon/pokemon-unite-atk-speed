148e5dcc6f85db3585fa3a010c4dc0d3080c2dd6
mazzon <filipe.mazzon.ribeiro@gmail.com>
2023-06-15 15:11:16 -0300

    fix blissey and azumarill values

--- src/infrastructure/storage/pokemons/azumarill/statsBase.helper.ts
+++ src/infrastructure/storage/pokemons/azumarill/statsBase.helper.ts
@@ -6,7 +6,7 @@ export const getAzumarillStats = (): PokemonStats[] => {
     { level: 1, atkSpeed: 10 },
     { level: 2, atkSpeed: 10.67},
     { level: 3, atkSpeed: 11.43 },
-    { level: 4, atkSpeed: 12.31 },
+    { level: 4, atkSpeed: 15.31 },
     { level: 5, atkSpeed: 16.32 },
     { level: 6, atkSpeed: 17.48 },
     { level: 7, atkSpeed: 18.82 },
@@ -16,7 +16,7 @@ export const getAzumarillStats = (): PokemonStats[] => {
     { level: 11, atkSpeed: 26.52 },
     { level: 12, atkSpeed: 29.22},
     { level: 13, atkSpeed: 32.32 },
-    { level: 14, atkSpeed: 35.89 },
-    { level: 15, atkSpeed: 39.99 }
+    { level: 14, atkSpeed: 35.9 },
+    { level: 15, atkSpeed: 40.0 }
   ];
 };
